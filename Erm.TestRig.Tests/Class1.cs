4e8f4f41ecd186e4260f1c8e5e5c9f4bd3d37600
BorjaRodrigo <borja.rubio@dm-companies.com>
2014-02-21 05:07:04 -0500

    ERM-9 Project Creation
    ERM-9 Project Creation

---
+++ Erm.TestRig.Tests/Class1.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erm.TestRig.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual("Hello World", "Hello World");
+        }
+    }
+}
---
+++ Erm.TestRig/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Erm.TestRig.WebApiApplication" Language="C#" %>
